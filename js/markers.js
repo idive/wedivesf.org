815200b05c5b744c434204017523851dc7b94274
Christopher Knox <chris@dragonfly.co.nz>
2013-07-25 07:06:28 +1200

    actually push

--- js/markers.js
+++ js/markers.js
@@ -3,7 +3,17 @@
   var markers = $.getJSON('/data/data.json', function(data) {
     data.forEach(function(d, i) {
       if (d.latlon) {
-        L.marker(d.latlon, {title: d.name}).addTo(map).bindPopup("<h3>" + d.name +"</h3>").openPopup();
+        L.marker(d.latlon, {title: d.name})
+          .addTo(map)
+          .bindPopup("<h3>" + d.name +"</h3>")
+          .openPopup()
+          .on("click", function(e) {
+            $('#infochild').remove();
+            $('#infoarea').append('<div id="infochild"/>');
+            $('#infochild').append("<h3>" + d.name + "</h3>")
+              .append('<p><a href="' + d.url + '">Homepage</a></p>');
+
+          });
       }
     });
   });
